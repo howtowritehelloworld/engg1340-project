89921a6c2ef03a6dd5374977fc36ea9a43f7a791
howtowritehelloworld <andyyishing23@gmail.com>
2024-04-12 17:45:34 +0800

    Implement readmap function

--- map.h
+++ map.h
@@ -2,6 +2,7 @@
 #define MAP_H
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "tower.h"
 #include "enemy.h"
@@ -10,5 +11,6 @@
 
 void printmap(char map[9][16]);
 void updatemap(char map[9][16], pathtile*& pathhead, std::vector<struct tower*> towers);
+void readmap(char map[9][16], int map_num);
 
 #endif // MAP_H
