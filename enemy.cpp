f5bb6db10f8de00d4cd7096d7c7fd9dfa0e89ee8
WenSzeHoi <160105905+WenSzeHoi@users.noreply.github.com>
2024-04-16 15:47:39 +0800

    Update enemy.cpp
    added weight

--- enemy.cpp
+++ enemy.cpp
@@ -5,7 +5,8 @@ void enemy::knight()
 {
     name = "Knight";
     icon = 'K';
-    health = 14;
+    health = 6;
+    weight = 2;
     type[0] = "Ground";
 }
 
@@ -13,7 +14,8 @@ void enemy::dragon()
 {
     name = "Dragon";
     icon = 'D';
-    health = 12;
+    health = 9;
+    weight = 3;
     type[0] = "Air";
 }
 
@@ -21,6 +23,7 @@ void enemy::ghost()
 {
     name = "Ghost";
     icon = 'G';
-    health = 9;
+    health = 6;
+    weight = 3;
     type[0] = "Ground"; type[1] = "Camo";
 }
