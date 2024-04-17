1338ab32f9b2bf9eedd0fdf29481a1318bc35c42
suncyric <162400258+suncyric@users.noreply.github.com>
2024-04-17 19:11:07 +0800

    Update save.h

--- map/save.h
+++ map/save.h
@@ -1,6 +1,13 @@
 #ifndef SAVEGAME_H
 #define SAVEGAME_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "tower.h"
+#include "enemy.h"
+#include "path.h"
+
 void saveGame();
 void loadGame();
 
