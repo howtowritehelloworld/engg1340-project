1700b611799728c2e854503a94770cbdfb169b20
WenSzeHoi <160105905+WenSzeHoi@users.noreply.github.com>
2024-04-17 11:37:17 +0800

    Update tower.cpp

--- tower.cpp
+++ tower.cpp
@@ -1,37 +1,37 @@
 #include "tower.h"
 
 using namespace std;
-void tower::mage()
+void tower::mage(int level)
 {
     name = "Mage";
     color_id = 3;
     icon = 'M';
     cost = 4;
-    damage = 2;
+    damage = 2*level;
     attackspeed = 2;
     range = 1;
     type[0] = "AOE"; type[1] = "Ground"; type[2] = "Air";
 }
 
-void tower::archer()
+void tower::archer(int level)
 {
     name = "Archer";
     color_id = 4;
     icon = 'A';
     cost = 3;
-    damage = 3;
+    damage = 3*level;
     attackspeed = 1;
     range = 1;
     type[0] = "Single"; type[1] = "Ground"; type[2] = "Air";     
 }
 
-void tower::sniper()
+void tower::sniper(int level)
 {
     name = "Sniper";
     color_id = 5;
     icon = 's';
     cost = 5;
-    damage = 6;
+    damage = 6*level;
     attackspeed = 3;
     range = 2;
     type[0] = "Single"; type[1] = "Ground"; type[2] = "Air"; type[3] = "Camo";
@@ -43,7 +43,7 @@ void tower::cannon()
     color_id = 6;
     icon = 'C';
     cost = 4;
-    damage = 4;
+    damage = 4*level;
     attackspeed = 3;
     range = 1;
     type[0] = "AOE"; type[1] = "Ground";
