85cd11557567d4d5643afbf3fc070a0353933f5d
suncyric <162400258+suncyric@users.noreply.github.com>
2024-04-17 19:05:20 +0800

    Create save.cpp

---
+++ map/save.cpp
@@ -0,0 +1,10 @@
+#include "savegame.h"
+#include <iostream>
+
+void saveGame() {
+    std::cout << "Saving the game..." << std::endl;
+}
+
+void loadGame() {
+    std::cout << "Loading the game..." << std::endl;
+}
