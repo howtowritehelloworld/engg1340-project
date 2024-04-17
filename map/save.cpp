96d63b0759bc7a0c556afc292ce54043012cb74a
suncyric <162400258+suncyric@users.noreply.github.com>
2024-04-17 21:30:59 +0800

    Update save.cpp

--- map/save.cpp
+++ map/save.cpp
@@ -2,8 +2,24 @@
 #include <iostream>
 #include <fstream>
 
-void saveGame() {
-    
+
+void saveGame(const std::vector &map) {
+    std::ofstream savefile("save.txt");
+    if (savefile.is_open()) { 
+        for (int row = 0; row < map.size(); row++) {
+            for (int col = 0; col < map[row].size(); col++) {
+                if (is_tower) {
+                    savefile <<; 
+                } else {
+                    savefile <<; 
+                }
+            }
+            outputFile << "\n"; // Move to the next line after each row
+        }
+
+        outputFile.close();
+        
+    }
 }
 
 void loadGame() {
