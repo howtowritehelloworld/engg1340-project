933dd4f81f6b11f35bdadea09e934c02ba60d93a
rtween1 <162346389+rtween1@users.noreply.github.com>
2024-04-18 15:00:13 +0800

    Update tower.h

--- tower.h
+++ tower.h
@@ -12,19 +12,43 @@ struct tower{
     std::string name;
     int color_id;
     char icon;
-    int level;
     int cost;
     int damage;
     int attackspeed;
-    int cd = 0;
     int range;
     std::string type[5];
     std::vector<struct tile*> tiles_covered;
+    int last = tiles_covered.size() - 1;
+    void mage(int level);
+    void archer(int level);
+    void sniper(int level);
+    void cannon(int level);
 
-    void mage(int tower_level);
-    void archer(int tower_level);
-    void sniper(int tower_level);
-    void cannon(int tower_level);
-};
+    void CalculateDamage(){
+      if (type[0] == "AOE"){
+        for (int i = 0; i < tiles_covered.size(); i++){
+          if (tiles_covered[i]->is_enemy() == true){
+            if (tiles_covered[i]->enemy_on_top->type[0] == "Air" && type[2] == ""){
+              continue;
+            }
+            else if (tiles_covered[i]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
+              continue;
+            }
+            tiles_covered[i]->enemy_on_top->health -= damage;
+          }
+        }
+      }
+      else if (type[0] == "Single"){
+        if (tiles_covered[last]->is_enemy() == true){
+          if (tiles_covered[last]->enemy_on_top->type[0] == "Air" && type[2] == ""){
+        }
+          else if (tiles_covered[last]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
+          }
+          else{
+            tiles_covered[last]->enemy_on_top->health -= damage;
+          }
+      }
+    }
+    };
 
 #endif
