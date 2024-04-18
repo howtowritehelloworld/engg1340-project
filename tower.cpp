96e3bb4e0f87b1a7fe87afb2076358bdc2604f83
Andy <andyyishing23@gmail.com>
2024-04-18 22:08:53 +0800

    Fix formatting

--- tower.cpp
+++ tower.cpp
@@ -53,3 +53,29 @@ void tower::cannon(int tower_level)
     type[0] = "AOE"; type[1] = "Ground"; type[2] = ""; type[3] = "";
 }
 
+void tower::CalculateDamage(){
+    if (type[0] == "AOE"){
+        for (int i = 0; i < tiles_covered.size(); i++){
+            if (tiles_covered[i]->is_enemy() == true){
+                if (tiles_covered[i]->enemy_on_top->type[0] == "Air" && type[2] == ""){
+                    continue;
+                }
+                else if (tiles_covered[i]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
+                continue;
+                }
+            tiles_covered[i]->enemy_on_top->health -= damage;
+            }
+        }
+    }
+    else if (type[0] == "Single"){
+        if (tiles_covered[last]->is_enemy() == true){
+            if (tiles_covered[last]->enemy_on_top->type[0] == "Air" && type[2] == ""){
+            }
+            else if (tiles_covered[last]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
+            }
+            else{
+                tiles_covered[last]->enemy_on_top->health -= damage;
+            }
+        }
+    }
+}
--- tower.h
+++ tower.h
@@ -12,6 +12,7 @@ struct tower{
     std::string name;
     int color_id;
     char icon;
+    int level;
     int cost;
     int damage;
     int attackspeed;
@@ -24,31 +25,7 @@ struct tower{
     void sniper(int level);
     void cannon(int level);
 
-    void CalculateDamage(){
-      if (type[0] == "AOE"){
-        for (int i = 0; i < tiles_covered.size(); i++){
-          if (tiles_covered[i]->is_enemy() == true){
-            if (tiles_covered[i]->enemy_on_top->type[0] == "Air" && type[2] == ""){
-              continue;
-            }
-            else if (tiles_covered[i]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
-              continue;
-            }
-            tiles_covered[i]->enemy_on_top->health -= damage;
-          }
-        }
-      }
-      else if (type[0] == "Single"){
-        if (tiles_covered[last]->is_enemy() == true){
-          if (tiles_covered[last]->enemy_on_top->type[0] == "Air" && type[2] == ""){
-        }
-          else if (tiles_covered[last]->enemy_on_top->type[1] == "Camo" && type[3] == ""){
-          }
-          else{
-            tiles_covered[last]->enemy_on_top->health -= damage;
-          }
-      }
-    }
-    };
+    void CalculateDamage();
+};
 
 #endif
