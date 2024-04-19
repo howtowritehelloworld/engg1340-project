12d4e51b92878ed1d551b8fc6a16e8ffd812236b
Andy <andyyishing23@gmail.com>
2024-04-19 22:20:19 +0800

    Implement lose screen

--- test.cpp
+++ test.cpp
@@ -105,7 +105,6 @@ void printMap(WINDOW *mainBox, tile map[9][16]) // Print the map of the game ins
     }
 }
 
-
 int main(int agrc, char **argv) 
 {
     initscr();
@@ -220,6 +219,7 @@ void print_enemy(WINDOW *win, tile*& path_start){
 }
 
 
+
 int mainmenu(WINDOW *win)
 {   
     std::string title1 = " _____                        ____        __                     ";
@@ -275,6 +275,87 @@ int mainmenu(WINDOW *win)
     }
 
     return highlight+1;
+}
+
+int loseScreen(int highlight = 0, int count = 1) {
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    std::string title1 = " __   __  _______  __   __    ___      _______  _______  _______ ";
+    std::string title2 = "|  | |  ||       ||  | |  |  |   |    |       ||       ||       |";
+    std::string title3 = "|  |_|  ||   _   ||  | |  |  |   |    |   _   ||  _____||    ___|";
+    std::string title4 = "|       ||  | |  ||  |_|  |  |   |    |  | |  || |_____ |   |___ ";
+    std::string title5 = "|_     _||  |_|  ||       |  |   |___ |  |_|  ||_____  ||    ___|";
+    std::string title6 = "  |   |  |       ||       |  |       ||       | _____| ||   |___ ";
+    std::string title7 = "  |___|  |_______||_______|  |_______||_______||_______||_______|";
+
+    int titleX = (xMax - title1.length()) / 2 - 3;
+
+    mvprintw(yMax / 2 - 7, titleX, title1.c_str());
+    mvprintw(yMax / 2 - 6, titleX, title2.c_str());
+    mvprintw(yMax / 2 - 5, titleX, title3.c_str());
+    mvprintw(yMax / 2 - 4, titleX, title4.c_str());
+    mvprintw(yMax / 2 - 3, titleX, title5.c_str());
+    mvprintw(yMax / 2 - 2, titleX, title6.c_str());
+    mvprintw(yMax / 2 - 1, titleX, title7.c_str());
+
+    refresh();
+
+    WINDOW* menuwin = newwin(7, xMax - 12, yMax - 10, 5);
+    box(menuwin, 0, 0);
+
+
+    keypad(menuwin, true);
+
+    std::string choices[2] = {"Main Menu", "Quit"};
+    int choice;
+
+    while (1) {
+  
+        for (int i = 0; i < 2; i++) {
+            if (i == highlight)
+                wattron(menuwin, A_REVERSE);
+            mvwprintw(menuwin, i + 2, (xMax - 17) / 2, choices[i].c_str());
+            wattroff(menuwin, A_REVERSE);
+        }
+
+
+        wrefresh(menuwin);
+
+
+        choice = wgetch(menuwin);
+
+        switch (choice) {
+            case KEY_UP:
+                highlight--;
+                if (highlight == -1) {
+                    highlight = 0;
+                }
+                break;
+            case KEY_DOWN:
+                highlight++;
+                if (highlight == 2) {
+                    highlight = 1;
+                }
+                break;
+            default:
+                break;
+        }
+
+        // When pressing Enter
+        if (choice == 10) {
+            // If the user chooses the main menu
+            if (highlight == 0) {
+                erase();
+                return 0;
+            }
+            // If the user chooses to quit
+            else if (highlight == 1) {
+                erase();
+                return 3;
+            }
+        }
+    }
 }
 
 int playscreen(WINDOW *win)
@@ -375,7 +456,7 @@ int playscreen(WINDOW *win)
                     wrefresh(statsBox);
                     
                     i++;
-                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
                     attack_all(map);
                     move(map, path_start, killed_enemies, health);
                 }
@@ -472,9 +553,13 @@ int playscreen(WINDOW *win)
         wrefresh(towerBox);
         wrefresh(statsBox);
         wrefresh(confirmBox);
+        if (health <= 0){
+            clear();
+            refresh();
+            break;
+        }
     }
-
-    return 0;
+    return loseScreen();
 }
 
 int helpscreen(WINDOW *win)
