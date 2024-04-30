3aeb525c2bbe77c6a49954cecf8bfc76a4f9fe7f
WenSzeHoi <160105905+WenSzeHoi@users.noreply.github.com>
2024-04-30 13:21:42 +0800

    Update tower.cpp

--- src/tower.cpp
+++ src/tower.cpp
@@ -31,6 +31,18 @@ void tower::mage(int tower_level)
 
 void tower::archer(int tower_level)
 {
+     /*
+
+    Input:
+    tower_level : Level of the tower to be created
+
+    Process:
+    Sets the attributes for an archer tower according to tower_level
+
+    Output:
+    Modified struct tower
+
+    */
     
     name = "Archer";
     color_id = 4;
